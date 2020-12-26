ec86bf4024d0b0cdbe79c74d58a5701a4d4919cb
Bedmano <nike-22@list.ru>
2020-12-26 19:12:08 +0300

    Улучшаем скорость загрузки

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector(".bar__toggle");
 const nav = document.querySelector(".main-nav");
+const slider = document.querySelector(".slider");
 const after = document.querySelector(".slider__after");
 const before = document.querySelector(".slider__before");
 const afterButton = document.querySelector(".slider-bar__text--after");
@@ -16,20 +17,21 @@ if (screen.width < 767) {
     menu.classList.toggle("bar__toggle--cross");
     nav.classList.toggle("main-nav--nojs");
   });
+  if (slider) {
+    afterButton.addEventListener("click", () => {
+      after.classList.add("slider--current");
+      before.classList.remove("slider--current");
+      sliderItemAfter.classList.add("slider-list__item--current");
+      sliderItemBefore.classList.remove("slider-list__item--current");
+    });
 
-  afterButton.addEventListener("click", () => {
-    after.classList.add("slider--current");
-    before.classList.remove("slider--current");
-    sliderItemAfter.classList.add("slider-list__item--current");
-    sliderItemBefore.classList.remove("slider-list__item--current");
-  });
-
-  beforeButton.addEventListener("click", () => {
-    after.classList.remove("slider--current");
-    before.classList.add("slider--current");
-    sliderItemAfter.classList.remove("slider-list__item--current");
-    sliderItemBefore.classList.add("slider-list__item--current");
-  });
+    beforeButton.addEventListener("click", () => {
+      after.classList.remove("slider--current");
+      before.classList.add("slider--current");
+      sliderItemAfter.classList.remove("slider-list__item--current");
+      sliderItemBefore.classList.add("slider-list__item--current");
+    });
+  }
 }
 
 // const sliderButton = document.querySelectorAll(".slider-bar__text");
